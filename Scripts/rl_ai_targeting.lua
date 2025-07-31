4f691ca83afd09da2b7658bc4268746bb4f18df1
VTrider <157431961+VTrider@users.noreply.github.com>
2025-07-30 21:01:11 -0400

    update ai targeting

--- Scripts/rl_ai_targeting.lua
+++ Scripts/rl_ai_targeting.lua
@@ -13,6 +13,8 @@ local vsp = require("vsp")
 
 local rl_ai_targeting = {}
 do
+    local smart_targeting_enabled = false
+
     function rl_ai_targeting.set_smart_targeting()
         
     end
@@ -22,9 +24,17 @@ do
     local excluded_odfs = vsp.set.make_set()
     local excluded_obj_classes = vsp.set.make_set()
 
+    --- Check if an object is allowed to use smart targeting
+    --- @param h Handle
+    --- @return boolean
+    local function can_smart_target(h)
+        return not excluded_odfs:contains(GetOdf(h)) and not excluded_obj_classes:contains(GetClassLabel(h))
+    end
+
     local function register_starting_objects()
+        if not smart_targeting_enabled then return end
         for _, obj in ipairs(vsp.object_service.get_start_objects():get()) do
-            if not excluded_odfs:contains(GetOdf(obj)) and not excluded_obj_classes:contains(GetClassLabel(obj)) then
+            if can_smart_target(obj) then
                 my_ai_units:insert(obj)
             end
         end
